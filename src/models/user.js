41755104b6d203bd228fe6e0c24691ea79b98ca6
Burak Karaman <karaman.buraak@gmail.com>
2024-07-20 11:05:54 +0200

    Add morgan middleware, update user model with new fields and password validation, and modify password encryption

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@
     NODEJS EXPRESS | FlightAPI
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
+const { CustomError } = require("../errors/customError");
 const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
 
@@ -18,7 +19,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      set: (password) => passwordEncrypt(password),
+      // set: (password) => passwordEncrypt(password),
     },
     email: {
       type: String,
@@ -30,9 +31,17 @@ const UserSchema = new mongoose.Schema(
         "Email type is not correct.",
       ],
     },
+    avatar: {
+      type: String,
+      trim: true,
+    },
     isActive: {
       type: Boolean,
       default: true,
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
     },
     isAdmin: {
       type: Boolean,
@@ -44,5 +53,60 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+//* create işleminde çalışacak
+UserSchema.pre("validate", function (next) {
+  console.log(this.password);
+  if (
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}])[A-Za-z\d!-\*?+&%{}]{8,}$/.test(
+      this.password
+    )
+  ) {
+    next();
+  } else {
+    throw new CustomError("Password type is incorrect!", 400);
+  }
+});
+
+UserSchema.pre("save", function (next) {
+  this.password = passwordEncrypt(this.password);
+  next();
+});
+
+//? update için
+const updateEncryptValidatePassword = function (next) {
+  const update = this.getUpdate();
+  if (update.password) {
+    if (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}])[A-Za-z\d!-\*?+&%{}]{8,}$/.test(
+        update.password
+      )
+    ) {
+      update.password = passwordEncrypt(update.password);
+    } else {
+      throw new CustomError("Password type is incorrect!", 400);
+    }
+  }
+
+  next();
+};
+
+// UserSchema.pre("updateOne", function (next) {
+//   const update = this.getUpdate();
+//   if (update.password) {
+//     if (
+//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}])[A-Za-z\d!-\*?+&%{}]{8,}$/.test(
+//         update.password
+//       )
+//     ) {
+//       update.password = passwordEncrypt(update.password);
+//     }else {
+//       throw new CustomError("Password type is incorrect!", 400);
+//     }
+//   }
+
+//   next()
+// });
+UserSchema.pre("updateOne", updateEncryptValidatePassword);
 
 module.exports = mongoose.model("User", UserSchema);
